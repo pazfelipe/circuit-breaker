cf36b7576833418d799a98c008ffcfb0fa343028
Felipe Paz <xxfelipepaz@gmail.com>
2024-06-27 16:35:18 -0300

    Add main Flask application code

---
+++ python_api/app/main.py
@@ -0,0 +1,17 @@
+# app/main.py
+from flask import Flask, jsonify
+import os
+import time
+
+app = Flask(__name__)
+
+
+@app.route("/status", methods=["GET"])
+def status():
+    return jsonify({"status": "Python API is running"}), 200
+
+
+if __name__ == "__main__":
+    # Sleep for a specified interval before starting the app (to simulate the restart)
+    time.sleep(int(os.environ.get("RESTART_INTERVAL", 10)))
+    app.run(host="0.0.0.0", port=5000)
